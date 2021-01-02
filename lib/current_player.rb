26880919f47264bebee84d62fd70e59352560e2a
Elliott Vargo <vargoelliott@gmail.com>
2021-01-02 21:30:18 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,8 +13,8 @@ end
 
 def current_player(board)
 if turn_count(board) % 2 == true
-   current_player(board) = "O"
+   current_player = "O"
 else
-  current_player(board) = "X"
+  current_player = "X"
 end
 end
